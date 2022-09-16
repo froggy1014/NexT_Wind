9b4a7758d550fbbb9004d8f58bf02e774f5b8fc1
froggy1014 <clcl6084@gmail.com>
2022-09-16 10:17:52 +0900

    style: tailwind initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(56, 189, 248, 0.3)',
+      }
+    },
+  },
+  plugins: [],
+}
